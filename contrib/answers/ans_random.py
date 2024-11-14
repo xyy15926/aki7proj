1c16afa34e439a2878ffa1782ec03f8a9056ea12
xyy15926 <xyy15926@163.com>
2024-11-14 22:07:29 +0800

    [update] update mcmc

--- contrib/answers/ans_random.py
+++ contrib/answers/ans_random.py
@@ -3,7 +3,7 @@
 #   Name: random.py
 #   Author: xyy15926
 #   Created: 2023-12-29 14:01:54
-#   Updated: 2024-01-12 17:06:07
+#   Updated: 2024-11-14 22:05:35
 #   Description:
 # ---------------------------------------------------------
 
@@ -17,9 +17,10 @@ except ImportError:
 from collections.abc import Iterator, Callable
 import logging
 
-from random import uniform, gauss
-from math import sin, cos, pi, sqrt, log
+from random import uniform, normalvariate
+from math import sin, cos, pi, sqrt, log, exp
 
+# %%
 logging.basicConfig(
     format="%(module)s: %(asctime)s: %(levelname)s: %(message)s",
     level=logging.INFO,
@@ -30,11 +31,6 @@ logger.info("Logging Start.")
 
 
 # %%
-def gen_uniform() -> Iterator:
-    while True:
-        yield uniform(0, 1)
-
-
 def box_muller() -> Iterator:
     while True:
         a = uniform(0, 1)
@@ -43,22 +39,85 @@ def box_muller() -> Iterator:
         yield sin(2 * pi * a) * sqrt(-2 * log(b))
 
 
-uni_gen = gen_uniform()
-norm_gen = box_muller()
-
-
 # %%
-def mcmc(
-    prob: Callable,
+def metropolis(
+    pdf: Callable,
     N: int = 10000,
     start: float = 0
 ) -> list:
+    """Metropolis Sampling.
+
+    Use the gauss distribution as the transition probability for the
+    followings:
+    1.1 The gauss distribution ranges from -inf to inf.
+    1.2 It's easy to draw samples sujecting to gauss distribution and the
+      conditional gausss distribution.
+    Attention: This orginal metropolis algorithm is sensetive to the
+      initialization for the precesion of the `alpha`, which may lead to
+      slow convergence to balance status or even stop during the loops.
+
+    Params:
+    ---------------------------
+    pdf: Target probability density function.
+    N: The number of samples.
+    start: The first element of the sequence.
+
+    Return:
+    ---------------------------
+    List of samples.
+    """
+    def guass_pdf(x):
+        return 1 / sqrt(2 * pi) * exp(-0.5 * x**2)
     rands = [0] * N
+    # Init the first element of the sequence.
     last = start
     for i in range(N):
-        new = next(norm_gen) + last
-        rj = next(uni_gen)
-        if rj < min(1, prob(new) / prob(last)):
+        # Draw sample subject to conditional gauss distribution.
+        new = normalvariate(0, 1) + last
+        # Draw accept-rejection prob.
+        rj = uniform(0, 1)
+        # Calcuate the acception rate.
+        alpha = guass_pdf(new - last) * pdf(new)
+        # Determine to accept or reject sample.
+        if rj <= alpha:
+            last = new
+        rands[i] = last
+    return rands
+
+
+def metropolis_hastings(
+    pdf: Callable,
+    N: int = 10000,
+    start: float = 0
+) -> list:
+    """Metropolis-Hastings Sampling.
+
+    Use the gauss distribution as the transition probability for the
+    followings:
+    1.1 The gauss distribution ranges from -inf to inf.
+    1.2 It's easy to draw samples sujecting to gauss distribution and the
+      conditional gausss distribution.
+
+    Params:
+    ---------------------------
+    pdf: Target probability density function.
+    N: The number of samples.
+    start: The first element of the sequence.
+
+    Return:
+    ---------------------------
+    List of samples.
+    """
+    rands = [0] * N
+    # Init the first element of the sequence.
+    last = start
+    for i in range(N):
+        # Draw sample subject to conditional gauss distribution.
+        new = normalvariate(0, 1) + last
+        # Draw accept-rejection prob.
+        rj = uniform(0, 1)
+        # Determine to accept or reject sample.
+        if rj < min(1, pdf(new) / pdf(last)):
             last = new
         rands[i] = last
     return rands
--- tests/contrib/answers/test_ans_random.py
+++ tests/contrib/answers/test_ans_random.py
@@ -3,7 +3,7 @@
 #   Name: test_ans_random.py
 #   Author: xyy15926
 #   Created: 2024-01-12 16:51:26
-#   Updated: 2024-01-12 17:01:36
+#   Updated: 2024-11-14 22:06:19
 #   Description:
 # ---------------------------------------------------------
 
@@ -16,17 +16,16 @@ if __name__ == "__main__":
     from contrib.answers import ans_random
     reload(ans_random)
 
-from contrib.answers.ans_random import mcmc
+from contrib.answers.ans_random import metropolis_hastings
 
 
 # %%
-
 def test_mcmc():
-    def target_dist(x):
+    def gauss_pdf(x):
         mu = 8
         sigma = 3
         return 1 / sigma / np.sqrt(2 * np.pi) * np.exp(-0.5 * (x - mu)**2 / sigma ** 2)
 
-    rands = mcmc(target_dist, 20000)
+    rands = metropolis_hastings(gauss_pdf, 20000)
     assert np.isclose(np.mean(rands), 8, atol=1)
     assert np.isclose(np.std(rands), 3, atol=1)
