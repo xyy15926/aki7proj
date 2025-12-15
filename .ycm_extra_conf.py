a1157ee4a931fab45842804da36df4234840c3e8
xyy15926 <xyy15926@163.com>
2025-12-15 20:08:51 +0800

    [update] update project config

--- .ycm_extra_conf.py
+++ .ycm_extra_conf.py
@@ -3,11 +3,15 @@
 #   Name: .ycm_extra_conf.py
 #   Author: xyy15926
 #   Created: 2025-09-22 20:11:41
-#   Updated: 2025-09-22 20:15:50
+#   Updated: 2025-12-11 22:22:30
 #   Description:
 # ---------------------------------------------------------
 
 def Settings(**kwargs):
+    from pathlib import Path
     return {
-        "interpreter_path": ".pixi/envs/default/bin/python3",
+        "interpreter_path": str(
+            Path(__file__).parent
+            / ".pixi/envs/default/bin/python3"
+        ),
     }
